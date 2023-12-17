36c83c145536eff12135dc439a66ee3ae41de199
nymphe <nymphetamine71@gmail.com>
2023-12-17 20:09:14 +0900

    initial commit

---
+++ lib/constant/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+String uri = 'http://192.168.3.13:3000';
+
+const backgroundColor = Color.fromARGB(221, 178, 189, 241);
+const textColor = Colors.black;
+const buttonColor = Colors.teal;
+const unselectedItemColor = Colors.grey;
+const errorColor = Colors.red;
+const normalColor = Color.fromARGB(224, 255, 255, 255);
+const clickColor = Color.fromARGB(222, 155, 155, 5);
+const navbarColor = Color.fromARGB(181, 108, 128, 232);
